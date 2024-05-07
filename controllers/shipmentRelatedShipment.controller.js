6b7022c76632d82b956c279bc23ccd1872c3eb31
kenneth.nelson@cox.net <kenneth.nelson@cox.net>
2024-05-07 13:51:44 -0700

    Fixed issue with related shipments.
    App was through an error when there were no related shipments.

--- controllers/shipmentRelatedShipment.controller.js
+++ controllers/shipmentRelatedShipment.controller.js
@@ -12,23 +12,25 @@ const { logger } = require('../utils/logger.js');
 async function parseRelatedShipments(shipment) {
   const payload = [];
 
-  for (const relatedShipment of shipment.relatedShipments) {
-    const r = {
-      shipment_id: shipment.id,
-      related_shipment_id: relatedShipment.id
-    };
-
-    for (const identifierType of sourceIdentifierTypes) {
-      r[identifierType] = null;
-    }
-
-    if(relatedShipment.identifiers && Array.isArray(relatedShipment.identifiers)) {
-      for (const identifier of relatedShipment.identifiers) {
-        r[identifier.type.toLowerCase()] = identifier.value;
+  if (!shipment.relatedShipments || !Array.isArray(shipment.relatedShipments)) {
+    for (const relatedShipment of shipment.relatedShipments) {
+      const r = {
+        shipment_id: shipment.id,
+        related_shipment_id: relatedShipment.id
+      };
+  
+      for (const identifierType of sourceIdentifierTypes) {
+        r[identifierType] = null;
       }
+  
+      if(relatedShipment.identifiers && Array.isArray(relatedShipment.identifiers)) {
+        for (const identifier of relatedShipment.identifiers) {
+          r[identifier.type.toLowerCase()] = identifier.value;
+        }
+      }
+  
+      payload.push(r);
     }
-
-    payload.push(r);
   }
 
   return payload;
